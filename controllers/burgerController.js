b68adfbd9a0df43cbc6bf3a6e3f0247f00454a1a
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-02-10 22:08:04 -0600

    added controllers and connected to server

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,2 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function (data) {
+        const object = {
+            burgers: data
+        };
+        console.log(object);
+        res.render("index", object);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create("name", req.body.name, function(result) {
+        res.json({ id: result.insertID });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+
+    burger.update("devoured", condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        };
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgerController.js");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -9,4 +10,10 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main "}));
 app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
 
