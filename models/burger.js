2160d7a460246bca699e79304d71fa986f4ab240
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-02-10 21:44:52 -0600

    added burger models

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,21 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-    
-}
+    all: function(cb) {
+        orm.all("burger", function(res) {
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb){
+        orm.create("burger", cols, vals, function(res) {
+            cb(res);
+          });
+    },
+    update: function(cols, condition, cb) {
+        orm.update("burger", cols, condition, function(res) {
+            cb(res);
+          });
+    }
+};
+
+module.exports = burger;
