af0d8b226a45c8425e7ae2c0b06a886ffeea8aef
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-02-10 22:25:25 -0600

    fixed typo on server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const routes = require("./controllers/burgerController.js");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.urlencoded({ etended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main "}));
